9b406e033df8f2d019936ab711be7b5809e84774
s30402 <s30402@pjwstk.edu.pl>
2024-06-09 13:33:03 +0200

    Changes to methods - added Savining and Loading from memory. - added Add() - added Subtract() - added Multiply() - added Divide() and countermeasure for dividing by 0

--- src/main/java/methods/menu.java
+++ src/main/java/methods/menu.java
@@ -2,7 +2,34 @@ package src.main.java.methods;
 
 public class menu {
 
+    private double saved = 0;
     private double base;
 
+    public void Save() {
+        this.saved = this.base;
+    }
 
+    public double Load() {
+        return this.saved;
+    }
+
+    public double Add(double i) {
+        return this.base + i;
+    }
+
+    public double Subtract(double i) {
+        return this.base - i;
+    }
+
+    public double Multiply(double i) {
+        return this.base * i;
+    }
+
+    public double Divide(double i) {
+        if(i != 0) {
+            return this.base / i;
+        } else {
+            throw new IllegalArgumentException("You can not divide by 0! :(");
+        }
+    }
 }
